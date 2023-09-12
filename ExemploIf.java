900099e3a0d1f0bf7ca0ac2d94724c38af92d868
Amanda Di Stadio <amandinhanino10@hotmail.com>
2023-09-11 21:24:16 -0300

    ap(03): implementacao if

---
+++ ExemploIf.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class ExemploIf{
+    static public void main (String[]args){
+    //declaração de vari[aveis
+    //entrada
+    double nota = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota"));
+    //processamento
+    //saída
+    if(nota >=70)
+    JOptionPane.showMessageDialog(null, "Aprovado");
+    
+    }
+}
